0f70410d3dee37e1c00b34b033d32cda6df09c45
atulshaw <atul26@unibit.in>
2026-05-09 13:11:05 +0530

    feat: initialize React root and disable console logging in production environment

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if (import.meta.env.PROD) {
+  console.log = () => {};
+  console.debug = () => {};
+  console.info = () => {};
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
